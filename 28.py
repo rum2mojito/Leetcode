23bdf7c5ba795b3a7b8972abd0b4905bd8bd0350
Stone <wead3105@sjtu.edu.cn>
2018-02-12 18:50:54 +0800

    Create 28.py

---
+++ 28.py
@@ -0,0 +1,10 @@
+// 28. Implement strStr()
+
+class Solution:
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        return haystack.find(needle)
