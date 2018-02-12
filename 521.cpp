b7ff711e2ba74cf05cceede3151607e051a68abd
Stone <wead3105@sjtu.edu.cn>
2018-02-12 16:17:17 +0800

    Create 521.cpp

---
+++ 521.cpp
@@ -0,0 +1,12 @@
+// 521. Longest Uncommon Subsequence I
+
+class Solution {
+public:
+    int findLUSlength(string a, string b) {
+        if (a == b)
+            return -1;
+        else {
+            return (a.length()>=b.length()? a.length():b.length());
+        }
+    }
+};
