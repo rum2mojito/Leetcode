88c4fbb0d489b957737cb617d3ec5f6c257c805f
Stone <wead3105@sjtu.edu.cn>
2018-02-13 18:57:40 +0800

    Create 136.py

---
+++ 136.py
@@ -0,0 +1,14 @@
+// 136. Single Number
+
+class Solution:
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        dic = {}
+        for num in nums:
+            dic[num] = dic.get(num, 0)+1
+        for key, val in dic.items():
+            if val == 1:
+                return key
