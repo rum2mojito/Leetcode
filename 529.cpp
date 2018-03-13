0f01a1937aa21c3d5efaf7cd13b0c8749ff664f3
Stone <wead3105@sjtu.edu.cn>
2018-03-13 22:41:23 +0800

    Update and rename 529-test.cpp to 529.cpp

--- 529.cpp
+++ 529.cpp
@@ -1,3 +1,5 @@
+529. Minesweeper
+
 class Solution {
 public:
     vector<vector<char>> updateBoard(vector<vector<char>>& board, vector<int>& click) {
@@ -86,7 +88,48 @@ public:
                 dfs (board,position);
                 position[1]++;
             }
+            // SW
+            if (position[0]+1<board.size() && position[1]-1>=0) {
+                if (board[position[0]+1][position[1]-1] == 'E') {
+                    position[1]--;
+                    position[0]++;
+                    dfs (board,position);
+                    position[1]++;
+                    position[0]--;
+                } 
+            }
+            // SE
+            if (position[0]+1<board.size() && position[1]+1<board[0].size()) {
+                if (board[position[0]+1][position[1]+1] == 'E'){
+                    position[1]++;
+                    position[0]++;
+                    dfs (board,position);
+                    position[1]--;
+                    position[0]--;
+                }
+            }
+            // NW
+            if (position[1]-1>=0 && position[0]-1>=0) {
+                if (board[position[0]-1][position[1]-1] == 'E') {
+                    position[1]--;
+                    position[0]--;
+                    dfs (board,position);
+                    position[1]++;
+                    position[0]++;
+                }
+            }
+            // NE
+            if (position[0]-1<board.size() && position[1]+1<board[0].size()) {
+                if (board[position[0]-1][position[1]+1] == 'E') {
+                    position[1]++;
+                    position[0]--;
+                    dfs (board,position);
+                    position[1]--;
+                    position[0]++;
+                }
+            }
         }
+        
         
         return;
     }
