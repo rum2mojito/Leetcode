6aabfa9792b7eeaa96d09eac253d6993e976b79f
Stone <wead3105@sjtu.edu.cn>
2018-03-13 10:46:06 +0800

    Create 529-test.cpp

---
+++ 529-test.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<vector<char>> updateBoard(vector<vector<char>>& board, vector<int>& click) {
+        if (board[click[0]][click[1]] == 'M') {
+            board[click[0]][click[1]] = 'X';
+            return board;
+        }
+        else if (board[click[0]][click[1]] == 'E') {
+            
+        }
+    }
+};
