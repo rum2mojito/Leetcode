06e28b0dee129e8f6cd745d117665fc1a3f2740b
Stone <wead3105@sjtu.edu.cn>
2018-03-15 16:57:07 +0800

    Create 406ing.cpp

---
+++ 406ing.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    vector<pair<int, int>> reconstructQueue(vector<pair<int, int>>& people) {
+        vector<vector<int>> res(people.size(),vector<int>(0);
+        for (int i=0;i<people.size();i++) {
+            res
+        }
+    }
+};
